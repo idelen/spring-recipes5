894e1c51c8f22209e29c1757b3de5c5410ef5f55
idelen <ch.jackpot@gmail.com>
2021-05-25 16:07:49 +0900

    [Macbook] recipe 2.2 finished

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 import com.apress.springrecipes.sequence.SequenceGenerator;
 import com.apress.springrecipes.sequence.config.SequenceGeneratorConfiguration;
 import com.apress.springrecipes.sequence.domain.SequenceDao;
+import com.apress.springrecipes.shop.Product;
+import com.apress.springrecipes.shop.config.ShopConfiguration;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -14,11 +16,20 @@ public class Main {
 //		System.out.println(generator.getSequence());
 //		System.out.println(generator.getSequence());
 
-		ApplicationContext context = new AnnotationConfigApplicationContext("com.apress.springrecipes.sequence");
+//		ApplicationContext context = new AnnotationConfigApplicationContext("com.apress.springrecipes.sequence");
+//
+//		SequenceDao sequenceDao = context.getBean(SequenceDao.class);
+//
+//		System.out.println(sequenceDao.getNextValue("IT"));
+//		System.out.println(sequenceDao.getNextValue("IT"));
 
-		SequenceDao sequenceDao = context.getBean(SequenceDao.class);
+		// recipe 2-2
+		ApplicationContext context = new AnnotationConfigApplicationContext(ShopConfiguration.class);
 
-		System.out.println(sequenceDao.getNextValue("IT"));
-		System.out.println(sequenceDao.getNextValue("IT"));
+		Product aaa = context.getBean("aaa", Product.class);
+		Product cdrw = context.getBean("cdrw", Product.class);
+
+		System.out.println(aaa);
+		System.out.println(cdrw);
 	}
 }
