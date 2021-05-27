488a65ea365e0e3ae3a3411dc5c0a23228e5a5a2
idelen <ch.jackpot@gmail.com>
2021-05-27 18:36:59 +0900

    [Macbook] backup

--- src/com/apress/springrecipes/sequence/SequenceGenerator.java
+++ src/com/apress/springrecipes/sequence/SequenceGenerator.java
@@ -1,8 +1,13 @@
 package com.apress.springrecipes.sequence;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class SequenceGenerator {
+
+	private PrefixGenerator prefixGenerator;
 
 	private String prefix;
 	private String suffix;
@@ -10,6 +15,10 @@ public class SequenceGenerator {
 	private final AtomicInteger counter = new AtomicInteger();
 
 	public SequenceGenerator() {
+	}
+
+	public void setPrefixGenerator(PrefixGenerator prefixGenerator) {
+		this.prefixGenerator = prefixGenerator;
 	}
 
 	public void setPrefix(String prefix) {
---
+++ src/com/apress/springrecipes/sequence/service/SequenceService.java
@@ -0,0 +1,23 @@
+package com.apress.springrecipes.sequence.service;
+
+import com.apress.springrecipes.sequence.domain.Sequence;
+import com.apress.springrecipes.sequence.domain.SequenceDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SequenceService {
+
+	@Autowired
+	private SequenceDao sequenceDao;
+
+	public void setSequenceDao(SequenceDao sequenceDao) {
+		this.sequenceDao = sequenceDao;
+	}
+
+	public String generate(String sequenceId) {
+		Sequence sequence = sequenceDao.getSequence(sequenceId);
+		int value = sequenceDao.getNextValue(sequenceId);
+		return sequence.getPrefix() + value + sequence.getSuffix();
+	}
+}
